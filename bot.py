b9210e00d4a1918295f6a8e93e936b6794d0831e
syetolog2011-sudo <syetolog.2011@gmail.com>
2026-01-13 18:27:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,22 @@
-print("Bot started")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+
+TOKEN = "8383475541:AAEIGuu-Cs-Cf7nim40BxFgo8hvBl-0MQ_g"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer(
+        "🎰 Добро пожаловать в кейсы!\n\n"
+        "Здесь ты можешь открывать кейсы и выигрывать ⭐"
+    )
+
+async def main():
+    print("Bot started")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
